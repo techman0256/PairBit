43af03288138b6e031966a7463ad7a3b0ca799aa
techman0256 <patelpranav916@gmail.com>
2025-12-13 18:20:13 +0530

    environment vars

--- apps/PairBit-UI/src/socket.ts
+++ apps/PairBit-UI/src/socket.ts
@@ -1,7 +1,9 @@
 import { io, Socket } from "socket.io-client";
 
 
-const socket: Socket = io("http://localhost:3000/editor", {
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+const socket: Socket = io(`${API_BASE_URL}/editor`, {
   autoConnect: false,
 });
 
