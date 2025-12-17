55d26aab631980878acc83bb2a1d4d290a7eae9d
techman0256 <patelpranav916@gmail.com>
2025-12-17 20:06:15 +0530

    extension clean code

---
+++ apps/PairBit-UI/src/extension/cursorTooltip.ts
@@ -0,0 +1,57 @@
+import {EditorState, StateField} from "@codemirror/state"
+import { EditorView } from "codemirror";
+import type { Tooltip } from "@codemirror/view";
+import {showTooltip} from "@codemirror/view"
+
+// Cursor tooltip behaviour can be changed from here
+export const getCursorTooltips = (state: EditorState): readonly Tooltip[] => {
+  return state.selection.ranges
+    .filter(range => range.empty)
+    .map(range => {
+      let line = state.doc.lineAt(range.head)
+      let text = line.number + ":" + (range.head - line.from)
+      return {
+        pos: range.head,
+        above: true,
+        strictSide: true,
+        arrow: true,
+        create: () => {
+          let dom = document.createElement("div")
+          dom.className = "cm-tooltip-cursor"
+          dom.textContent = text
+          return {dom}
+        }
+      }
+    })
+}
+
+const cursorTooltipField = StateField.define<readonly Tooltip[]>({
+  create: getCursorTooltips,
+
+  update(tooltips, tr) {
+    if (!tr.docChanged && !tr.selection) return tooltips
+    return getCursorTooltips(tr.state)
+  },
+
+  provide: f => showTooltip.computeN([f], state => state.field(f))
+});
+
+const cursorTooltipBaseTheme = EditorView.baseTheme({
+  ".cm-tooltip.cm-tooltip-cursor": {
+    backgroundColor: "#66b",
+    color: "white",
+    border: "none",
+    padding: "2px 7px",
+    borderRadius: "4px",
+    "& .cm-tooltip-arrow:before": {
+      borderTopColor: "#66b"
+    },
+    "& .cm-tooltip-arrow:after": {
+      borderTopColor: "transparent"
+    }
+  }
+})
+
+export const cursorTooltip = () => {
+  return [cursorTooltipField, cursorTooltipBaseTheme]
+}
